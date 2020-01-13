d087b8647b31906b9bd9fd0c052692b1b5c882fa
Moi <jeremy@quasiaqui.fr>
2020-01-13 22:03:59 +0100

    init : webpack

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+const $ = require('jquery');
+
+require('bootstrap');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
+const imagesContext = require.context('../img', true, /\.(png|jpg|jpeg|gif|ico|svg|webp)$/);
+imagesContext.keys().forEach(imagesContext);
+
+// any CSS you require will output into a single css file (app.css in this case)
+require('../scss/_variables.scss');
+require('../scss/app.scss');
