f03d06b07e82044ad0a21e8a3aa50e6096d367c3
Moi <jeremy@quasiaqui.fr>
2020-01-14 19:55:28 +0100

    feat : authentication

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\Type\EditAccountType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AccountController extends AbstractController
+{
+
+    /**
+     * @IsGranted("ROLE_USER")
+     * @Route("/edition-compte", name="edit_account", methods={"GET", "POST"})
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return Response
+     */
+    public function edit(Request $request, UserPasswordEncoderInterface $passwordEncoder) : Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(EditAccountType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('account/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
