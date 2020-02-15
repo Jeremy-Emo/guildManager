30f8f879c145cb9c1a4099d6f53561628b38b1b1
Unknown <jeremy@quasiaqui.fr>
2020-02-15 17:33:58 +0100

    fix : command

--- src/Command/CreateUserCommand.php
+++ src/Command/CreateUserCommand.php
@@ -2,6 +2,8 @@
 
 namespace App\Command;
 
+use App\Entity\Guild;
+use App\Entity\Members;
 use App\Entity\User;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -28,9 +30,10 @@ class CreateUserCommand extends Command
     protected function configure()
     {
         $this
-            ->setDescription('Permet la création d\'un utilisateur')
+            ->setDescription('Permet la création d\'un utilisateur admin lié à une guilde')
             ->addArgument('name', InputArgument::REQUIRED, "Le nom de l'utilisateur")
             ->addArgument('password', InputArgument::REQUIRED, "Le mot de passe de l'utilisateur")
+            ->addArgument('idGuilde', InputArgument::REQUIRED, "L\id de la guilde")
         ;
     }
 
@@ -38,12 +41,20 @@ class CreateUserCommand extends Command
     {
         $em = $this->container->get('doctrine')->getManager();
         $name = $input->getArgument('name');
+        $idGuilde = $input->getArgument('idGuilde');
+
+        $guild = $this->container->get('doctrine')->getRepository(Guild::class)->find($idGuilde);
 
         $user = new User();
         $user->setUsername($name);
         $pwd = $this->passwordEncoder->encodePassword($user, $input->getArgument('password'));
-        $user->setPassword($pwd);
+        $user->setPassword($pwd)->setIsAdmin(true);
         $em->persist($user);
+
+        $member = new Members();
+        $member->setGuild($guild)->setUser($user);
+        $em->persist($member);
+
         $em->flush();
 
         return 0;
